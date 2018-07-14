97fd0d4a7bb5d5d4a1f672db3d5d261b1ce3f0d1
Hanbi Choi <personabe1984@gmail.com>
2018-07-14 20:28:13 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,14 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   organized_hash = {}
+
+   data.each do |attributes, options|
+     options.each do |option, name|
+       pigeons.each do |name|
+         organized_hash[name] ||= {}
+         organized_hash[name][attributes] ||= []
+         organized_hash[name][attributes] << option.to_s
+       end
+     end
+   end
+   return organized
 end
