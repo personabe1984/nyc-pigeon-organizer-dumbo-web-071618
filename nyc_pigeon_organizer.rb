4d55fdbe95466745a06ac8624a4b2bc5f9ccdaed
Hanbi Choi <personabe1984@gmail.com>
2018-07-14 20:22:44 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,2 +1,15 @@
 def nyc_pigeon_organizer(data)
+   organized = {}
+
+   data.each do |trait, options|
+     options.each do |option, pigeons|
+       pigeons.each do |pigeon|
+         organized[pigeon] ||= {}
+         organized[pigeon][trait] ||= []
+         organized[pigeon][trait] << option.to_s
+       end
+     end
+   end
+
+   organized
 end
